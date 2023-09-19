3dbfd989673cf93e0afbc6965043636220a820e2
petruzl <leandro.petruz@gmail.com>
2023-09-19 05:32:14 -0300

    cóigo até o grafico sma

--- media_movel.py
+++ media_movel.py
@@ -5,3 +5,22 @@ import plotly.graph_objects as go
 ativo = '^BVSP'
 dados = yf.download(ativo, start='2022-01-01', end='2023-08-01')
 dados
+periodo_sma = 20
+dados['sma'] = ta.trend.sma_indicator(dados['Close'], window=periodo_sma)
+dados
+dados.head(30)
+fig = go.Figure()
+df_fig = dados
+fig.add_trace(go.Candlestick(
+    x=df_fig.index,
+    open=df_fig['Open'],
+    high=df_fig['High'],
+    low=df_fig['Low'],
+    close=df_fig['Close'],
+    name='Grafico de Candlesticks com SMA'))
+fig.add_trace(go.Scatter(name='sma',
+                         x=df_fig.index,
+                         y=dados['sma'],
+                         marker_color='black'))
+fig.update_layout(template='ggplot2', width=1400, height=700)
+fig.show()
